ba75d0b678fc709481be13d5c4dcf48e1ef8bfe7
YangLong <2336739721@qq.com>
2020-01-08 12:46:58 +0800

    深搜学习完了  全排列  n的k次方

--- C,C++/蓝桥杯视频总结/DFS/抽象dfs/K个数和为sum2.cpp
+++ C,C++/蓝桥杯视频总结/DFS/抽象dfs/K个数和为sum2.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int n, k, sum, ans;
 int a[110], xuan[110];
 
-void dfs(int s,int cnt){
+void dfs(int s,int cnt, int pos){
 	if(cnt == k){
 		if(s == sum){
 			ans ++;
@@ -16,10 +16,10 @@ void dfs(int s,int cnt){
 	if(cnt > k || s > sum){
 		return;
 	}
-	for(int i=0;i<n;i++){
+	for(int i=pos;i<n;i++){
 		if(!xuan[i]){
 			xuan[i] = 1;
-			dfs(s+a[i], cnt+1);
+			dfs(s+a[i], cnt+1, i+1);
 			xuan[i] = 0;
 		}
 	}
@@ -32,7 +32,7 @@ int main(){
 		cin >> a[i];
 	}
 	ans = 0;
-	dfs(0, 0);
+	dfs(0, 0, 0);
 	cout << ans << endl;
 	return 0;
 }
@@ -41,6 +41,11 @@ int main(){
 之前的方法是每次去抉择是否选择第i个数，现在我们的策略是从剩下的数中选择一个数。 
 比如有5个数1,2,3,4,5,如果选择了1,那么剩下2,3, 4, 5四个数;如果选择了2,那么剩下1,3,4,5四个数，
 还可以选择....选择....选择....
+
+5 3 9
+1 2 3 4 5
+
+2
 
 1 3 5, 3 5 1, 1 5 3 会变成三种情况 
 
